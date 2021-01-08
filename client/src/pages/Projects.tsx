4e16ce20df8e6efe2be82d85ee33cb20bad1b8f1
kschmitz123 <kathrin_schmitz93@gmx.de>
2021-01-08 15:58:51 +0100

    Add first project component

---
+++ client/src/pages/Projects.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import InfoBlock from "../components/InfoBlock";
+import ListItems from "../components/ListItems";
+
+export default function Projects() {
+  return (
+    <InfoBlock width={"95%"}>
+      <h2>Tech Stack</h2>
+      <ListItems />
+    </InfoBlock>
+  );
+}
