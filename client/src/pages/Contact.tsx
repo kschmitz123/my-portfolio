f36f7bf94fc494103fa485127f3e9ecdfa3b5ca8
kschmitz123 <kathrin_schmitz93@gmx.de>
2021-01-08 14:52:10 +0100

    Add contact page

---
+++ client/src/pages/Contact.tsx
@@ -0,0 +1,63 @@
+import React from "react";
+import InfoBlock from "../components/InfoBlock";
+import GitHub from "../assets/GitHub-Mark-64px.png";
+import LinkedIn from "../assets/LI-In-Bug-64px.png";
+import Email from "../assets/email-64px.png";
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  div {
+    display: flex;
+    align-items: center;
+    font-weight: bold;
+  }
+  a {
+    text-decoration: none;
+    color: var(--main-color);
+  }
+  img {
+    margin-right: 10px;
+  }
+`;
+
+export default function Contact() {
+  return (
+    <Container>
+      <InfoBlock>
+        <h2>Hier bin ich erreichbar:</h2>
+      </InfoBlock>
+      <InfoBlock>
+        <a href="https://github.com/kschmitz123" target="blank">
+          <img src={GitHub} alt="GitHub Logo" />
+        </a>
+        <a href="https://github.com/kschmitz123" target="blank">
+          https://github.com/kschmitz123
+        </a>
+      </InfoBlock>
+      <InfoBlock>
+        <a
+          href="https://www.linkedin.com/in/kathrin-schmitz-5b477812b/"
+          target="blank"
+        >
+          <img src={LinkedIn} alt="LinkedIn Logo" />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/kathrin-schmitz-5b477812b/"
+          target="blank"
+        >
+          https://www.linkedin.com/in/kathrin-schmitz-5b477812b/
+        </a>
+      </InfoBlock>
+      <InfoBlock>
+        <a href="mailto:kathrin_schmitz93@gmx.de">
+          <img src={Email} alt="Email Logo" />
+        </a>
+        <p>kathrin_schmitz93 at gmx.de</p>
+      </InfoBlock>
+    </Container>
+  );
+}
