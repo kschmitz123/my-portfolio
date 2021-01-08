867b1185975f344054e3be5ff6416a364ff5a8f0
kschmitz123 <kathrin_schmitz93@gmx.de>
2021-01-08 15:59:03 +0100

    Add type check to infoblock

--- client/src/components/InfoBlock.tsx
+++ client/src/components/InfoBlock.tsx
@@ -2,20 +2,25 @@ import React, { ReactNode } from "react";
 import styled from "styled-components";
 import Background from "../assets/background-test.png";
 
-const Container = styled.div`
+interface Props {
+  width?: string;
+}
+
+const Container = styled.div<Props>`
   border-radius: 15px;
   box-shadow: 4px 8px 19px -6px rgba(0, 0, 0, 0.75);
-  width: 50%;
+  width: ${(props) => props.width || "50%"};
   color: var(--main-color);
   padding: 10px;
   margin: 30px;
   background: url(${Background}) no-repeat center center fixed;
 `;
 
-interface Props {
+interface Children {
   children: ReactNode;
+  width?: string;
 }
 
-export default function InfoBlock({ children }: Props) {
-  return <Container>{children}</Container>;
+export default function InfoBlock({ children, width }: Children) {
+  return <Container width={width}>{children}</Container>;
 }
