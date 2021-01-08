0a8317ff850c2c090fae9963e8685f3180f5ee7d
kschmitz123 <kathrin_schmitz93@gmx.de>
2021-01-08 12:07:09 +0100

    Add homepage

---
+++ client/src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Jumbotron from "../components/Jumbotron";
+import ProjectPreview from "../components/ProjectPreview";
+
+export default function Home() {
+  return (
+    <div>
+      <Jumbotron />
+      <ProjectPreview />
+    </div>
+  );
+}
