d0b7fffb13194139934ece2adf7bf062b8b7216d
kschmitz123 <kathrin_schmitz93@gmx.de>
2021-01-08 15:30:51 +0100

    Add route for projects

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,6 +5,7 @@ import Header from "./components/Header";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Home from "./pages/Home";
+import Projects from "./pages/Projects";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Route exact path="/" component={Home} />
       <Route path="/about" component={About} />
       <Route path="/contact" component={Contact} />
+      <Route path="/projects" component={Projects} />
       <Footer />
     </Router>
   );
