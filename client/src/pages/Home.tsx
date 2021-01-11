b81c3d08aebcc6d0bb05af950e33b832006ee08b
kschmitz123 <kathrin_schmitz93@gmx.de>
2021-01-11 11:02:44 +0100

    Change project preview on home page

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,12 +1,40 @@
 import React from "react";
 import Jumbotron from "../components/Jumbotron";
 import ProjectPreview from "../components/ProjectPreview";
+import gif from "../assets/frame-with-video.gif";
+import ListItems from "../components/ListItems";
 
 export default function Home() {
   return (
     <div>
       <Jumbotron />
-      <ProjectPreview />
+      <ProjectPreview>
+        <img src={gif} alt="" />
+        <span>
+          <h3> CRAFTIFIED</h3>
+          <p>
+            For all DIY Geeks who seek inspiration for the next project or want
+            to upload their own to share it with the community.
+          </p>
+          <ListItems />
+          <div>
+            <a
+              href="https://github.com/kschmitz123/diy-project-app"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Repository
+            </a>
+            <a
+              href="https://craftified.herokuapp.com/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              App
+            </a>
+          </div>
+        </span>
+      </ProjectPreview>
     </div>
   );
 }
